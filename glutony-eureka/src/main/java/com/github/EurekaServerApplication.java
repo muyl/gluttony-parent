57cd0e16b5d271fb2d7668692521eddce2ce6675
仲拓 <muylzh@163.com>
2020-05-10 13:18:17 +0800

    feat(新增kafka配置):
    新增kafka调用实例

---
+++ glutony-eureka/src/main/java/com/github/EurekaServerApplication.java
@@ -0,0 +1,29 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author 拓仲 on 2020/5/10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+}
